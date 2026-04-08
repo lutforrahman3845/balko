3e313da143ad1ee5bc1a33624fec2b6e8a22d92e
MD.Lutfor Rahman <lutforr3845@gmail.com>
2026-04-09 00:07:29 +0600

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "randomuser.me",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
       },
     ],
   },
